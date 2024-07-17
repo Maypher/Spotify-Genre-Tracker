92b47c0ec191bda0a9cf9fec53d8ed6c1ee53301
Maypher <maypheroffcial@gmail.com>
2024-07-17 16:42:39 -0400

    Fix bug where error messages when loging in were printed in the wrong color

--- cli/__init__.py
+++ cli/__init__.py
@@ -4,33 +4,39 @@ from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.table import Table
 from rich import box
+from rich.style import Style
 
 import sys
 
 class Program:
-    ERROR_STYLE = "red"
-    SUCCESS_STYLE = "green"
+    SUCCESS_STYLE = Style(color="green")
+    ERROR_STYLE = Style(color="red")
 
     console: Console
     backend: Client
-    refresh_token: str | None
 
     def __init__(self, refresh_token: str | None = None) -> None:
         self.console = Console()
-        self.backend = Client()
-        self.refresh_token = refresh_token
+        if refresh_token:
+            self.backend = Client(refresh_token)
+        else:
+            self.backend = Client()
 
     def run(self):
         self.print_spotify_logo()
         
         while True:
-            if self.refresh_token:
+            # If a refresh token exists it means the user is authenticated
+            if self.backend.authenticator.refresh_token:
                 # Print full options
                 pass
             else:
                 self.anonymous_prompt_menu()
 
     def anonymous_prompt_menu(self) -> int:
+        """
+        Prompt menu used when no API token is available. 
+        """
         menu = Table(box=box.HEAVY)
         menu.title = "Available Commands"
         menu.show_lines = True
@@ -48,14 +54,17 @@ class Program:
 
         match option:
             case 1:
+                # Authenticate against api
                 self.backend.authenticator.request_auth_code()
-                redirected_url = Prompt.ask("Enter redirect URL: ")
+                redirected_url = Prompt.ask("Enter redirect URL")
                 try:
                     self.backend.authenticator.request_access_token(redirected_url)
-                except PermissionError as e:
-                    self.console.print(e, style=self.ERROR_STYLE)
+                except (PermissionError, KeyError) as e:
+                    # Use e.args[0] because for some reason the error gets printed between single quotes which makes it green.
+                    # By doing it this way the double quotes are removed.
+                    self.console.print(e.args[0], style=Program.ERROR_STYLE)
                 else:
-                    self.console.print("Authentication successful", style=self.SUCCESS_STYLE)
+                    self.console.print("Authentication successful", style=Program.SUCCESS_STYLE)
             case 2:
                 self.console.print("Goodbye!")
                 sys.exit()
