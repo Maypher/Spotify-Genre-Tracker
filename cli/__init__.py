7abd15fbc1558eb3d981ba7648c6470939c95243
Maypher <maypheroffcial@gmail.com>
2024-07-20 17:38:25 -0400

    Add authenticated prompt menu and listen loop thead

--- cli/__init__.py
+++ cli/__init__.py
@@ -6,8 +6,9 @@ from rich.prompt import Prompt, IntPrompt
 from rich.table import Table
 from rich import box
 from rich.style import Style
+import threading
 
-import sys
+import time
 
 class Program:
     SUCCESS_STYLE = Style(color="green")
@@ -28,6 +29,9 @@ class Program:
 
     def run(self):
         self.print_spotify_logo()
+
+        with DatabaseManager() as db:
+            db.migration_upgrade()
         
         while not self.finish:
             # If a refresh token exists it means the user is authenticated
@@ -37,7 +41,7 @@ class Program:
             else:
                 self.anonymous_prompt_menu()
 
-    def anonymous_prompt_menu(self) -> int:
+    def anonymous_prompt_menu(self):
         """
         Prompt menu used when no API token is available. 
         """
@@ -45,7 +49,7 @@ class Program:
         menu.title = "Available Commands"
         menu.show_lines = True
 
-        menu.add_column("ID", style="red")
+        menu.add_column("ID", style="blue")
         menu.add_column("Name")
         menu.add_column("Description")
 
@@ -69,8 +73,41 @@ class Program:
                     self.console.print(e.args[0], style=Program.ERROR_STYLE)
                 else:
                     self.console.print("Authentication successful", style=Program.SUCCESS_STYLE)
+                    self._start_listening_thread() # Starts the thread that constantly updates the database
             case 2:
                 self.console.print("Goodbye!")
+                self.finish = True
+
+    def prompt_menu(self):
+        """
+        Prompt menu for authenticated user.
+        """
+
+        menu = Table(title="Available Commands", box=box.HEAVY)
+        
+        menu.add_column("ID", style="blue")
+        menu.add_column("Name")
+        menu.add_column("Description")
+
+        menu.add_row("1", "View progress", "Displays the progress of all genres.")
+        menu.add_row("2", "View genre(s) progress", "Displays the progress of the genres that match the given query.")
+        menu.add_row("3", "Logout", "Logs out by removing the credentials of the authenticated user. (TODO)")
+        menu.add_row("4", "Quit", "Terminates the program")
+
+        option = IntPrompt.ask(choices=["1", "2", "3", "4"])
+
+        match option:
+            case 1:
+                # Get and display the progress of all genres
+                pass
+            case 2:
+                # View a specific genre's progress
+                pass
+            case 3:
+                # Logout
+                pass
+            case 4:
+                # Quit
                 self.finish = True
 
     def print_spotify_logo(self):
@@ -113,15 +150,70 @@ class Program:
                    
         """, style="#1ED760")
 
-    def _update_genre_listen_time(self, increment_by: int):
+    def _update_genre_listen_time(self, genre_id: int, increment_by: int):
         """
         Updates the listen time of the genre of the currently playing song.
         """
 
         with DatabaseManager() as db:
-            db.increment_genre_listen_time(increment_by)
-    
-    def currently_listening_update_loop(self):
+            db.increment_genre_listen_time(genre_id, increment_by)
+
+    def _currently_listening_update_loop(self, check_interval: float):
         """
-        Made to be called from a different thread. Gets the currently playing song and updates it in the database
+        Made to be called from a different thread. Gets the currently playing song and updates it in the database.
+        
+        ---
+        # Parameters
+        - `check_interval`: How often to ping the player for the current song.
         """
+
+        recorded_song: CurrentTrack | None = self.backend.get_current_track()
+        recorded_time: int = recorded_song.progress_s if recorded_song else None # The time in seconds at the previous iteration of the loop
+
+        while not self.finish:
+            current_song: CurrentTrack | None = self.backend.get_current_track()
+
+            # If no song was found before the start of the loop set it to the current song
+            if not recorded_song:
+                recorded_song = current_song
+
+            # If the user hasn't changed the song since the previous iteration of the loop
+            if current_song:
+                # For the first iteration of the loop if no song is playing the set it to 0
+                if not recorded_time:
+                    recorded_time = current_song.progress_s
+
+                if current_song.song_title == recorded_song.song_title:
+                    current_time = current_song.progress_s
+                    dif_time = current_time - recorded_time # The difference in time between the current moment and the last loop iteration
+
+                    # The system could be cheated by manually forwarding the song.
+                    # This makes sure it doesn't happen by checking if the time difference is greater than the check interval
+                    # Multiplied by 2 to give some leeway to the request. 
+                    if (dif_time > 2 * check_interval):
+                        continue
+
+                    with DatabaseManager() as db:
+                        for genre in current_song.genres:
+                            genre_id = db.get_genres_by_name(genre)[0] # Since genre names are unique then it is assured that only one will be returned
+                            self._update_genre_listen_time(genre_id, dif_time)
+                    
+                    recorded_time = current_time
+                else:
+                    # If songs changed then the genres in common have to be updated
+                    common_genres = [genre for genre in current_song.genres if genre in recorded_song.genres]
+                    current_time = current_song.progress_s
+                    
+                    with DatabaseManager() as db:
+                        common_genres.append(current_song.genres)
+                        for genre in set(common_genres):
+                            genre_id = db.get_genres_by_name(genre)[0]
+                            self._update_genre_listen_time(genre_id, current_time)
+                    
+                    recorded_time = current_time
+
+            time.sleep(check_interval)
+
+    def _start_listening_thread(self):
+        listenting_thread = threading.Thread(target=self._currently_listening_update_loop, args=(1.5, ))
+        listenting_thread.start()
